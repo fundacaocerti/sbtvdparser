42bbf269ac810af4775c70d8434710fce785ef42
gabriel_marques <gag@certi.org.br>
2010-09-02 17:22:49 +0000

    Update notification mechanism and dialog

--- src/sys/Update.java
+++ src/sys/Update.java
@@ -15,22 +15,22 @@ public class Update extends Thread {
 			return;
 		Persistence.set(Persistence.LAST_UPDATE_CHECK, Long.toString(System.currentTimeMillis(), 16));
 		String[] info = null;
-		if (Persistence.get(Persistence.UI_LANG_IDIOM).equals("pt"))
-			info = getLatestVersionInfo("http://sbtvdparser.sourceforge.net/downloads_pt.htm");
+		if (Persistence.get(Persistence.UI_LANG_IDIOM).equals("pt")) //$NON-NLS-1$
+			info = getLatestVersionInfo("http://sbtvdparser.sourceforge.net/downloads_pt.htm"); //$NON-NLS-1$
 		else
-			info = getLatestVersionInfo("http://sbtvdparser.sourceforge.net/downloads.htm");
+			info = getLatestVersionInfo("http://sbtvdparser.sourceforge.net/downloads.htm"); //$NON-NLS-1$
 		if (info[0].compareToIgnoreCase(Persistence.get(Persistence.LAST_UPDATE_VERSION)) < 1)
 			return;
 		if (info[0].compareToIgnoreCase(Persistence.CURRENT_SW_VERSION) < 1) {
 			Persistence.set(Persistence.LAST_UPDATE_VERSION, Persistence.CURRENT_SW_VERSION);
 			return;
 		}
-		gui.dialogs.Update.open(info[0], info[1], info[2]);
+		gui.dialogs.Update.open(info[0], info[1], info[2], Long.parseLong(info[3], 16));
 	}
 
 	public String[] getLatestVersionInfo(String from) {
 		try {
-			String[] info = new String[3];
+			String[] info = new String[4];
 			URL url = new URL(from);
 			URLConnection cnx = url.openConnection();
 			cnx.setDoInput(true);
@@ -43,21 +43,26 @@ public class Update extends Thread {
 			boolean descStart = false;
 			while (line != null) {
 				if (descStart) {
-					line = line.replace("<br>", "\n");
-					description.append(line.replaceAll("<.+?>", ""));
-					if (line.indexOf("/p") > 0) {
+					line = line.replace("<br>", "\n"); //$NON-NLS-1$ //$NON-NLS-2$
+					description.append(line.replaceAll("<.+?>", "")); //$NON-NLS-1$ //$NON-NLS-2$
+					if (line.indexOf("/p") > 0) { //$NON-NLS-1$
 						descStart = false;
 						info[2] = description.toString().trim();
 						return info;
 					}
 				}
-				int href = line.indexOf("href");
+				if (line.indexOf("releasedate") > 0) { //$NON-NLS-1$
+					int hexbegin = line.indexOf('"', line.indexOf("value")) + 1; //$NON-NLS-1$
+					info[3] = line.substring(hexbegin, hexbegin + 11);
+				}
+
+				int href = line.indexOf("href"); //$NON-NLS-1$
 				if (href > 0) {
-					int urlStart = line.indexOf("\"", href) + 1;
-					int urlEnd = line.indexOf("\"", urlStart);
+					int urlStart = line.indexOf("\"", href) + 1; //$NON-NLS-1$
+					int urlEnd = line.indexOf("\"", urlStart); //$NON-NLS-1$
 					String dlUrl = line.substring(urlStart, urlEnd);
-					if (dlUrl.endsWith("download")) {
-						info[0] = line.substring(line.indexOf("(") + 2, line.indexOf(")"));
+					if (dlUrl.endsWith("download")) { //$NON-NLS-1$
+						info[0] = line.substring(line.indexOf("(") + 2, line.indexOf(")")); //$NON-NLS-1$ //$NON-NLS-2$
 						info[1] = dlUrl;
 						descStart = true;
 						description = new StringBuffer();
@@ -67,7 +72,6 @@ public class Update extends Thread {
 			}
 		} catch (MalformedURLException e) {
 		} catch (IOException e) {
-			System.out.println("Could not open download site.");
 		}
 		return null;
 	}
